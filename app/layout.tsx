d144230ca926ab9f12b6e90dae14e1829536dc84
EsinShadrach <esinshadrachrafe@gmail.com>
2024-08-21 02:16:50 +0100

    Added Animated dialog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ModalProvider } from "~/components/personals/modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#050814] dark`}>{children}</body>
+      <body className={`${inter.className} bg-[#050814] dark`}>
+        <ModalProvider>{children}</ModalProvider>
+      </body>
     </html>
   );
 }
--- components/personals/header.tsx
+++ components/personals/header.tsx
@@ -1,7 +1,45 @@
+"use client";
+import { motion, useAnimation } from "framer-motion";
+import { ClockIcon, RocketIcon } from "lucide-react";
 import Link from "next/link";
+import React from "react";
+import {
+  CloseIcon,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalTrigger,
+  useModal,
+} from "~/components/personals/modal";
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { TypographyH4 } from "./typography";
 
 export function Header() {
+  const [wallet, setwallet] = React.useState("");
+  const { setOpen } = useModal();
+  const controls = useAnimation();
+
+  function handleButtonClick() {
+    setOpen(false);
+
+    controls
+      .start({
+        scale: 1.5,
+        rotate: 360,
+        transition: { duration: 0.5, ease: "easeInOut" },
+      })
+      .then(() => {
+        controls.start({
+          y: -1000,
+          opacity: 0,
+          transition: { duration: 1, ease: "easeIn" },
+        });
+      });
+  }
+
   return (
     <header className="bg-foreground/10">
       <nav className="container flex justify-between p-3 mx-auto border-b">
@@ -11,9 +49,49 @@ export function Header() {
         >
           Ascendix
         </Link>
-        <Button className="" size={"lg"}>
-          Connect Wallet
-        </Button>
+
+        <Modal>
+          <Button className="" size={"lg"} asChild>
+            <ModalTrigger>Connect Wallet</ModalTrigger>
+          </Button>
+          <ModalBody className="bg-red-200">
+            <ModalContent>
+              <TypographyH4 className="text-center">
+                Add a Wallet to Ascendix
+              </TypographyH4>
+              <p>
+                <Label htmlFor="wallet" className="text-right">
+                  Wallet
+                </Label>
+                <Input
+                  id="wallet"
+                  value={wallet}
+                  placeholder="0x...1xx"
+                  className="mt-3"
+                  onChange={(e) => setwallet(e.target.value)}
+                />
+              </p>
+            </ModalContent>
+            <ModalFooter>
+              <CloseIcon>
+                <Button
+                  className="w-full overflow-hidden"
+                  onClick={handleButtonClick}
+                >
+                  Add Address
+                  <motion.div
+                    className="w-6 h-6 ml-2"
+                    animate={controls}
+                    initial={{ rotate: 0 }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                  >
+                    <RocketIcon />
+                  </motion.div>
+                </Button>
+              </CloseIcon>
+            </ModalFooter>
+          </ModalBody>
+        </Modal>
       </nav>
     </header>
   );
